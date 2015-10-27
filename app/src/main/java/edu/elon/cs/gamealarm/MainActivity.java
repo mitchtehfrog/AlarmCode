05b18182f52baf541213b6963eaa3bde224e2c2f
unknown <mwinkler3@CARPMAC-04.fs.elon.edu>
2015-10-27 09:39:14 -0400

    Switch setOnLongClickListener to setOnItemLongClickListener

--- app/src/main/java/edu/elon/cs/gamealarm/MainActivity.java
+++ app/src/main/java/edu/elon/cs/gamealarm/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends Activity {
         listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(itemClickListener);
-        listView.setOnLongClickListener(itemLongClickListener);
+        listView.setOnItemLongClickListener(itemLongClickListener);
 
     }
 
